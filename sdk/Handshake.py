24368ea75dde2db0a9f3cffed355c100bec05601
Logan Gorence <loganjohngorence@gmail.com>
2015-08-31 13:16:44 -0700

    Implement key saving.

--- sdk/Handshake.py
+++ sdk/Handshake.py
@@ -1,9 +1,11 @@
 import base64
 import hashlib
 import json
-import requests
+import os.path
+import pickle
 
 import pyelliptic
+import requests
 
 
 class Handshake:
@@ -28,11 +30,35 @@ class Handshake:
         })
 
     def run_handshake(self):
-        requests.post("http://localhost:8080/conn?dsId=" + self.get_dsid(), data=self.get_json())
+        response = requests.post("http://localhost:8080/conn?dsId=" + self.get_dsid(), data=self.get_json())
 
 class Keypair:
     def __init__(self):
-        self.keypair = pyelliptic.ECC(curve="prime256v1")
+        self.keypair = None
+        if not os.path.isfile(".keys"):
+            self.generate_key()
+            self.save_keys()
+        else:
+            self.load_keys()
         sha = hashlib.sha256(self.keypair.get_pubkey())
         self.b64 = base64.urlsafe_b64encode(sha.digest()).decode("utf-8").replace("=", "")
         self.encoded_public = base64.urlsafe_b64encode(self.keypair.get_pubkey()).decode("utf-8").replace("=", "")
+
+    def generate_key(self):
+        self.keypair = pyelliptic.ECC(curve="prime256v1")
+
+    def load_keys(self):
+        file = open(".keys", "rb")
+        keys = pickle.load(file)
+        self.keypair = pyelliptic.ECC(curve="prime256v1",
+                                      pubkey_x=keys["pubkey_x"],
+                                      pubkey_y=keys["pubkey_y"],
+                                      raw_privkey=keys["privkey"])
+
+    def save_keys(self):
+        file = open(".keys", "wb")
+        pickle.dump({
+            "pubkey_x": self.keypair.pubkey_x,
+            "pubkey_y": self.keypair.pubkey_y,
+            "privkey": self.keypair.privkey
+        }, file)
