1b5a273279b60d85630095a291a6802df5e085e8
Logan Gorence <loganjohngorence@gmail.com>
2015-11-21 20:01:40 -0800

    v0.5.1, fixes bugs, and changes the way some Node methods work.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.version_info < (3, 3):
 
 setup(
     name="dslink",
-    version="0.5.0",
+    version="0.5.1",
     description="DSLink SDK for Python",
     url="http://github.com/IOT-DSA/sdk-dslink-python",
     author="Logan Gorence",
