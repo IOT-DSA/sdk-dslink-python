7367cd9c043380da1868b172775cd89e3ad42c35
Logan Gorence <loganjohngorence@gmail.com>
2015-11-23 17:22:54 -0800

    Releasing typo fix, v0.5.7.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.version_info < (3, 3):
 
 setup(
     name="dslink",
-    version="0.5.6",
+    version="0.5.7",
     description="DSLink SDK for Python",
     url="http://github.com/IOT-DSA/sdk-dslink-python",
     author="Logan Gorence",
