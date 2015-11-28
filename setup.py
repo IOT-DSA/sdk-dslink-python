80aa0bdb937d7fcd9d83dfe833206eecb6a430fd
Logan Gorence <loganjohngorence@gmail.com>
2015-11-27 19:15:57 -0800

    Releasing v0.5.10, adds some optional warning/error messages.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.version_info < (3, 3):
 
 setup(
     name="dslink",
-    version="0.5.9",
+    version="0.5.10",
     description="DSLink SDK for Python",
     url="http://github.com/IOT-DSA/sdk-dslink-python",
     author="Logan Gorence",
