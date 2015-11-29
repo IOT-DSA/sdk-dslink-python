607be54cb0a2e90606d079d3469046d0f009f7fd
Logan Gorence <loganjohngorence@gmail.com>
2015-11-29 14:05:38 -0800

    Releasing v0.5.15, which supports value serialization and adds a bit more documentation to DSLink to indicate what you should and should not use.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setup(
     name="dslink",
-    version="0.5.14",
+    version="0.5.15",
     description="DSLink SDK for Python",
     url="http://github.com/IOT-DSA/sdk-dslink-python",
     author="Logan Gorence",
