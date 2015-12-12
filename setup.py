56f829ca8ec844d2e8a28ad56d8cd84904d56ad8
Logan Gorence <loganjohngorence@gmail.com>
2015-12-11 19:34:07 -0800

    Releasing bugfix v0.6.1.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setup(
     name="dslink",
-    version="0.6.0",
+    version="0.6.1",
     description="DSLink SDK for Python",
     url="http://github.com/IOT-DSA/sdk-dslink-python",
     author="Logan Gorence",
