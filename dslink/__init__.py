9f4f95ac1b40f3a0957b58213f04c2d232193d40
Logan Gorence <loganjohngorence@gmail.com>
2016-02-22 21:25:07 -0800

    Bugfix v0.6.3.

--- dslink/__init__.py
+++ dslink/__init__.py
@@ -7,7 +7,7 @@ from dslink.Response import *
 from dslink.Value import Value
 
 __title__ = "dslink"
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 __author__ = "Logan Gorence"
 
 __license__ = "Apache 2.0"
