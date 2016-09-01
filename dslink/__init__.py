8ed43eaeb211addf7dd320f249dc686c46e4449b
Logan Gorence <loganjohngorence@gmail.com>
2016-09-01 10:50:03 -0700

    v0.6.21.

--- dslink/__init__.py
+++ dslink/__init__.py
@@ -9,7 +9,7 @@ from .Storage import StorageDriver
 from .Value import Value
 
 __title__ = "dslink"
-__version__ = "0.6.20"
+__version__ = "0.6.21"
 __author__ = "Logan Gorence"
 
 __license__ = "Apache 2.0"
